8effdbd6617a0ee8af0461c3132651e2426dda14
Ondrej Komarek <ondrej.komarek@strv.com>
2017-02-15 14:12:19 +0100

    Initial commit, generated app by android studio, added multidex

---
+++ app/src/main/java/com/batterywatchface/ondrejkomarek/meetupwatchface/MeetupWatchfaceApplication.java
@@ -0,0 +1,37 @@
+package com.batterywatchface.ondrejkomarek.meetupwatchface;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+
+/**
+ * Created by ondrejkomarek on 15/02/2017.
+ */
+
+public class MeetupWatchfaceApplication  extends Application
+{
+	private static MeetupWatchfaceApplication sInstance;
+
+	public MeetupWatchfaceApplication()
+	{
+		sInstance = this;
+	}
+
+	public static Context getContext()
+	{
+		return sInstance;
+	}
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+	}
+
+	@Override
+	protected void attachBaseContext(Context base) {
+		super.attachBaseContext(base);
+		MultiDex.install(this);
+	}
+}
